c7db4553503c10a9b6f84054dd33ce3a67ca49eb
adamszabolcs <szabolcs.adam91@gmail.com>
2018-10-01 16:36:24 +0200

    if all-in, flop

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -5,13 +5,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "nullapontegypontketto";
+    static final String VERSION = "nullapontkettopontegy";
 
 
     public static int betRequest(JsonElement request) {
@@ -32,7 +30,9 @@ public class Player {
                         }
                     }
                 }
-
+                if (currentBuyIn >= player.getAsJsonObject().get("stack").getAsInt()) {
+                    currentBuyIn = 0;
+                }
             }
             System.err.println("\n Player: " + player + "\n");
         }
