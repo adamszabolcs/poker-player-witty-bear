eb6d31c3b8c81117ac533c8ef74f1bab66840acc
adamszabolcs <szabolcs.adam91@gmail.com>
2018-10-01 10:32:17 +0200

    betrquest started implementing

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,15 +1,19 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Player {
 
     static final String VERSION = "0.0.1";
 
+
     public static int betRequest(JsonElement request) {
-        return 0;
+        JsonElement root = new JsonParser().parse(request);
+        return 50;
     }
 
     public static void showdown(JsonElement game) {
