ef4cd549ba4ef58c1e56dbd205ebafadd574c19c
Clethcriss <balazs.borsi.1986@gmail.com>
2018-10-01 11:46:14 +0200

    ver 4

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,12 +6,12 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "0.0.3";
+    static final String VERSION = "0.0.4";
 
 
     public static int betRequest(JsonElement request) {
         Map<String,Object> result = new Gson().fromJson(request, Map.class);
-        Double currentBuyIn = (Double) result.get("minimum_buy_in");
+        Double currentBuyIn = (Double) result.get("current_buy_in");
         return currentBuyIn.intValue() + 50;
     }
 
