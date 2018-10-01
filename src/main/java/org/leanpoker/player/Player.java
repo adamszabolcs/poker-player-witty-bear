67a47ef528835f3183d8cec719b389c5ca344a68
Clethcriss <balazs.borsi.1986@gmail.com>
2018-10-01 15:26:44 +0200

    new tactic

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,19 +1,36 @@
 package org.leanpoker.player;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "0.0.4";
+    static final String VERSION = "nullapontegypontketto";
 
 
     public static int betRequest(JsonElement request) {
-        Map<String,Object> result = new Gson().fromJson(request, Map.class);
-        Double currentBuyIn = (Double) result.get("current_buy_in");
-        System.err.println(result.get("players"));
-        return currentBuyIn.intValue() + 50;
+        JsonObject object = request.getAsJsonObject();
+        JsonArray players = object.getAsJsonArray("players");
+        for (JsonElement player: players){
+            JsonArray playerHand = player.getAsJsonObject().get("hole_cards").getAsJsonArray();
+            if (player.getAsJsonObject().get("name").getAsString().equals("Player 1") && playerHand.size() == 2){
+                System.err.println("\nPlayer hand: " + player.getAsJsonObject().get("hole_cards") + "\n\n");
+                if (playerHand.get(0).getAsJsonObject().get("rank").equals(playerHand.get(1).getAsJsonObject().get("rank"))) {
+                    int stack = (int) player.getAsJsonObject().get("stack").getAsInt();
+                    return stack;
+                }
+            }
+        }
+        int currentBuyIn = (int) object.getAsJsonObject().get("current_buy_in").getAsInt();
+
+
+        return currentBuyIn + 50;
     }
 
     public static void showdown(JsonElement game) {
